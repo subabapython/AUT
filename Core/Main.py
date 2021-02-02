4ff2a9904642bd897a5ede2edb7d2513e54ef65c
subabapython <1450732536@qq.com>
2021-02-02 16:25:44 +0800

    main

--- Core/Main.py
+++ Core/Main.py
@@ -44,9 +44,10 @@ def runMain():
     result_path = _path(r"report\result")
     html_path = _path(r"report\html")
     allure_path = _path(r"allure-2.13.7\bin")
-    pytest.main(["-s", "-n", "4", case_path, "--alluredir", result_path])
-    command_str = fr"{allure_path}\allure generate {result_path} -o {html_path} --clean"
-    os.system(command_str)
+    pytest.main(["-s", "-n", "4", case_path])
+    #pytest.main(["-s", "-n", "4", case_path, "--alluredir", result_path])
+    #command_str = fr"{allure_path}\allure generate {result_path} -o {html_path} --clean"
+    #os.system(command_str)
 
 if __name__ == "__main__" :
     a = input()
